2f855c0c9cf0cf7591052d4ace43ed427d744812
Daniil Kovalev <kovdan01@yandex.ru>
2021-05-13 23:44:26 +0300

    Various changes

--- source/ak_128.c
+++ source/ak_128.c
@@ -6,11 +6,47 @@
 /* ----------------------------------------------------------------------------------------------- */
  #include <libakrypt.h>
 
+ inline int ak_128_equal( const ak_uint64 *x, const ak_uint64 *y )
+{
+  return ( x[0] == y[0] && x[1] == y[1] );
+}
+
+ inline int ak_128_is_zero( const ak_uint64 *x )
+{
+  return ( x[0] == 0 && x[1] == 0 );
+}
+
+ inline int ak_128_is_one( const ak_uint64 *x )
+{
+  return ( x[0] == 1 && x[1] == 0 );
+}
+
+ inline void ak_128_set_zero( ak_uint64 *x )
+{
+  x[0] = 0;
+  x[1] = 0;
+}
+
+ inline void ak_128_set_one( ak_uint64 *x )
+{
+  x[0] = 1;
+  x[1] = 0;
+}
+
 /* ----------------------------------------------------------------------------------------------- */
 /*                                  арифметические операции                                        */
 /* ----------------------------------------------------------------------------------------------- */
 
- ak_uint64 ak_128_add( ak_uint64 *z, const ak_uint64 *x, const ak_uint64 *y )
+/* ----------------------------------------------------------------------------------------------- */
+/*! Функция складывает два 128-битных числа и возвращает знак переноса. Результат сложения
+   помещается в переменную z. Указатель на z не может совпадать с указателями на слагаемые.
+
+    @param z Указатель на 128-битное число, в которое помещается результат
+    @param x Левый аргумент опреации сложения
+    @param y Правый аргумент операции сложения
+    @return Знак переноса (1, если x + y >= 2^128, 0 в противном случае)                           */
+/* ----------------------------------------------------------------------------------------------- */
+ inline ak_uint64 ak_128_add( ak_uint64 *z, const ak_uint64 *x, const ak_uint64 *y )
 {
   z[0] = x[0] + y[0];
   z[1] = x[1] + y[1];
@@ -19,14 +55,23 @@
   return ret;
 }
 
- void ak_128_add_mod( ak_uint64 *z, const ak_uint64 *x, const ak_uint64 *y, const ak_uint64 *p )
+/* ----------------------------------------------------------------------------------------------- */
+/*! Функция складывает два вычета по заданному 128-битному модулю. Результат сложения помещается
+    в переменную z. Указатель на z не может совпадать с указателями на слагаемые и модуль.
+
+    @param z Указатель на вычет, в который помещается результат
+    @param x Левый аргумент опреации сложения
+    @param y Правый аргумент операции сложения
+    @param p Модуль, по которому производятся вычисления                                           */
+/* ----------------------------------------------------------------------------------------------- */
+ inline void ak_128_add_mod( ak_uint64 *z, const ak_uint64 *x, const ak_uint64 *y, const ak_uint64 *p )
 {
   z[0] = x[0] + y[0];
   z[1] = x[1] + y[1];
-  int tmp = z[1] < x[1];
-  z[1] += (z[0] < x[0]);
+  ak_uint64 tmp = z[1] < x[1];
+  z[1] += z[0] < x[0];
 
-  // if sum is great or equal than p
+  // если сумма больше либо равна p
   tmp = (tmp == 1 ? ak_true : (z[1] < p[1] ? ak_false : (z[0] < p[0] ? ak_false : ak_true)));
   if (tmp == ak_false)
     return;
@@ -36,20 +81,67 @@
   z[1] -= tmp;
 }
 
+/* ----------------------------------------------------------------------------------------------- */
+/*! Функция вычитает одно 128-битное число из другого и возвращает знак переноса. Результат вычитания
+    помещается в переменную z. Указатель на z не может совпадать с указателями на операнды.
+
+    @param z Указатель на 128-битное число, в которое помещается результат
+    @param x Число, из которого происходит вычитание
+    @param y Вычитаемое
+    @return Знак переноса (1, если x < y, 0 в противном случае)                                    */
+/* ----------------------------------------------------------------------------------------------- */
+ inline ak_uint64 ak_128_sub( ak_uint64 *z, const ak_uint64 *x, const ak_uint64 *y )
+{
+  z[0] = x[0] - y[0];
+  z[1] = x[1] - y[1];
+  ak_uint64 ans = z[1] > x[1];
+  ak_uint64 before = z[1];
+  z[1] -= z[0] > x[0];
+  ans |= before < z[1];
+  return ans;
+}
+
+/* ----------------------------------------------------------------------------------------------- */
+/*! Функция реализует операцию вычитания двух вычетов по 128-битному модулю. Результат вычитания
+    помещается в переменную z. Указатель на z не может совпадать с указателями на операнды и модуль.
+
+    @param z Указатель на вычет, в который помещается результат
+    @param x Вычет, из которого происходит вычитание
+    @param y Вычитаемое
+    @param p Модуль, по которому производятся вычисления                                           */
+/* ----------------------------------------------------------------------------------------------- */
+ inline void ak_128_sub_mod( ak_uint64 *z, const ak_uint64 *x, const ak_uint64 *y, const ak_uint64 *p )
+{
+  if ( ak_128_sub( z, x, y ) )
+  {
+    z[0] += p[0];
+    z[1] += p[1];
+    z[1] += p[0] > z[0];
+  }
+}
+
 #define umul_ppmm(w1, w0, u, v) \
   __asm__ ("mulq %3" : "=a,a" (w0), "=d,d" (w1) : "%0,0" (u), "r,m" (v))
 
- void ak_128_mul( ak_uint64 *z, const ak_uint64 *x, const ak_uint64 *y )
+/* ----------------------------------------------------------------------------------------------- */
+/*! Функция перемножает два 128-битных числа. Результат умножения помещается в 256-битное число z.
+    Указатель на z не может совпадать с указателями на множители.
+
+    @param z Указатель на 256-битное число, в которое помещается результат
+    @param x Левый аргумент опреации умножения
+    @param y Правый аргумент операции умножения                                                    */
+/* ----------------------------------------------------------------------------------------------- */
+ inline void ak_128_mul( ak_uint64 *z, const ak_uint64 *x, const ak_uint64 *y )
 {
   ak_uint64 tmp;
 
-  ak_uint64 x0muly[2]; // два старших разряда, младший - сразу в z[0]
+  ak_uint64 x0muly[2];  // два старших разряда, младший - сразу в z[0]
   umul_ppmm( tmp, z[0], x[0], y[0] );
   umul_ppmm( x0muly[1], x0muly[0], x[0], y[1] );
   x0muly[0] += tmp;
   x0muly[1] += x0muly[0] < tmp;
 
-  ak_uint64 x1muly[2]; // два младших разряда, старший - сразу в z[3]
+  ak_uint64 x1muly[2];  // два младших разряда, старший - сразу в z[3]
   umul_ppmm( x1muly[1], x1muly[0], x[1], y[0] );
   umul_ppmm( z[3], tmp, x[1], y[1] );
   x1muly[1] += tmp;
@@ -62,6 +154,22 @@
   z[3] += tmp;
 }
 
+/* ----------------------------------------------------------------------------------------------- */
+/*! Функция перемножает два вычета по заданному 128-битному модулю. Результат умножения помещается
+    в переменную z. Указатель на z не может совпадать с указателями на множители и модуль.
+
+    @param z Указатель на вычет, в который помещается результат
+    @param x Левый аргумент опреации умножения
+    @param y Правый аргумент операции умножения
+    @param p Модуль, по которому производятся вычисления                                           */
+/* ----------------------------------------------------------------------------------------------- */
+ inline void ak_128_mul_mod( ak_uint64 *z, const ak_uint64 *x, const ak_uint64 *y, const ak_uint64 *p )
+{
+  ak_mpzn256 tmp;
+  ak_128_mul( tmp, x, y );
+  ak_128_rem( z, tmp, p );
+}
+
  static void rshift_1_bit( ak_uint64 *x )
 {
   x[0] >>= 1;
@@ -71,7 +179,7 @@
   x[2] >>= 1;
 }
 
- static ak_uint64 sub_2digits( ak_uint64 *x, ak_uint64 *y )
+ inline static ak_uint64 sub_2digits( ak_uint64 *x, const ak_uint64 *y )
 {
   if ( x[1] > y[1] )
   {
@@ -91,7 +199,7 @@
   return 0;
 }
 
- static ak_uint64 sub_3digits( ak_uint64 *x, ak_uint64 *y )
+ inline static ak_uint64 sub_3digits( ak_uint64 *x, const ak_uint64 *y )
 {
   if ( x[2] > y[2] )
   {
@@ -116,14 +224,22 @@
   return 0;
 }
 
- // assuming u < p * 2^128
+/* ----------------------------------------------------------------------------------------------- */
+/*! Функция получает остаток от деления 256-битного числа на 128-битный модуль. Результат помещается
+    в переменную r. Указатель на r не может совпадать с указателями на делимое и модуль.
+    Предполагается, что p >= 2^127.
+
+    @param r Указатель на вычет, в который помещается остаток
+    @param u Делимое
+    @param p Модуль, по которому производятся вычисления                                           */
+/* ----------------------------------------------------------------------------------------------- */
  void ak_128_rem( ak_uint64 *r, const ak_uint64 *u, const ak_uint64 *p )
 {
-  ak_uint64 pshifted[4] = { 0, 0, p[0], p[1] };
-  ak_uint64 x[4] = { u[0], u[1], u[2], u[3] };
+  ak_mpzn256 pshifted = { 0, 0, p[0], p[1] };
+  ak_mpzn256 x = { u[0], u[1], u[2], u[3] };
 
   // 0
-  // sub_2digits( x + 2, pshifted + 2 );
+  sub_2digits( x + 2, pshifted + 2 );
   pshifted[1] |= (ak_uint64)(pshifted[2] & 1) << 63;
   pshifted[2] >>= 1;
   pshifted[2] |= (ak_uint64)(pshifted[3] & 1) << 63;
@@ -168,14 +284,25 @@
   r[1] = x[1];
 }
 
- // assuming u < p * 2^128
- void ak_128_div( ak_uint64 *r, ak_uint64 *d, const ak_uint64 *u, const ak_uint64 *p )
+/* ----------------------------------------------------------------------------------------------- */
+/*! Функция получает частное и остаток от деления 256-битного числа на 128-битный модуль. Результат
+    помещается в переменную r. Указатель на r не может совпадать с указателями на делимое и модуль.
+    Предполагается, что p >= 2^127.
+
+    @param q Указатель на вычет, в который помещаются младшие 128 бит частного
+    @param r Указатель на вычет, в который помещается остаток
+    @param u Делимое
+    @param p Модуль, по которому производятся вычисления
+    @return Бит переноса (1, если p * 2^128 <= u, 0 в противном случае)                            */
+/* ----------------------------------------------------------------------------------------------- */
+ ak_uint64 ak_128_div( ak_uint64 *q, ak_uint64 *r, const ak_uint64 *u, const ak_uint64 *p )
 {
-  ak_uint64 pshifted[4] = { 0, 0, p[0], p[1] };
-  ak_uint64 x[4] = { u[0], u[1], u[2], u[3] };
-  d[0] = 0;
-  d[1] = 0;
+  ak_mpzn256 pshifted = { 0, 0, p[0], p[1] };
+  ak_mpzn256 x = { u[0], u[1], u[2], u[3] };
+  q[0] = 0;
+  q[1] = 0;
   // 0
+  ak_uint64 ret = sub_2digits( x + 2, pshifted + 2 );  // бит 2^128
   pshifted[1] |= (ak_uint64)(pshifted[2] & 1) << 63;
   pshifted[2] >>= 1;
   pshifted[2] |= (ak_uint64)(pshifted[3] & 1) << 63;
@@ -184,18 +311,18 @@
   // 1 - 62
   for ( int i = 0; i < 62; ++i )
   {
-    d[1] |= sub_3digits( x + 1, pshifted + 1 ) << (63 - i);
+    q[1] |= sub_3digits( x + 1, pshifted + 1 ) << (63 - i);
     rshift_1_bit( pshifted + 1 );
   }
 
   // 63
-  d[1] |= sub_3digits( x + 1, pshifted + 1 ) << 1;
+  q[1] |= sub_3digits( x + 1, pshifted + 1 ) << 1;
   pshifted[1] = p[0];
   pshifted[2] = p[1];
   pshifted[3] = 0;
 
   // 0
-  d[1] |= sub_3digits( x + 1, pshifted + 1 );
+  q[1] |= sub_3digits( x + 1, pshifted + 1 );
   pshifted[0] |= (ak_uint64)(pshifted[1] & 1) << 63;
   pshifted[1] >>= 1;
   pshifted[1] |= (ak_uint64)(pshifted[2] & 1) << 63;
@@ -204,27 +331,22 @@
   // 1 - 62
   for ( int i = 0; i < 62; ++i )
   {
-    d[0] |= sub_3digits( x, pshifted ) << (63 - i);
+    q[0] |= sub_3digits( x, pshifted ) << (63 - i);
     rshift_1_bit( pshifted );
   }
 
   // 63
-  d[0] |= sub_3digits( x, pshifted ) << 1;
+  q[0] |= sub_3digits( x, pshifted ) << 1;
   pshifted[0] = p[0];
   pshifted[1] = p[1];
   pshifted[2] = 0;
 
-  d[0] |= sub_3digits( x, pshifted );
+  q[0] |= sub_3digits( x, pshifted );
 
   r[0] = x[0];
   r[1] = x[1];
-}
 
- void ak_128_mul_mod( ak_uint64 *z, const ak_uint64 *x, const ak_uint64 *y, const ak_uint64 *p )
-{
-  ak_uint64 tmp[4];
-  ak_128_mul( tmp, x, y );
-  ak_128_rem( z, tmp, p );
+  return ret;
 }
 
  static void ak_128_gcd( const ak_uint64* a, const ak_uint64* b, ak_uint64 *x, ak_uint64 *y )
@@ -237,98 +359,84 @@
     y[1] = 0;
     return;
   }
-  ak_uint64 x1[4] = { 0, 0, 0, 0 };
-  ak_uint64 y1[4] = { 0, 0, 0, 0 };
-  ak_uint64 b_div_a[4] = { 0, 0, 0, 0 };
-  ak_uint64 b_mod_a[4] = { 0, 0, 0, 0 };
+  ak_mpzn256 x1 = { 0, 0, 0, 0 };
+  ak_mpzn256 y1 = { 0, 0, 0, 0 };
+  ak_mpzn256 b_div_a = { 0, 0, 0, 0 };
+  ak_mpzn256 b_mod_a = { 0, 0, 0, 0 };
 
-  ak_128_div( b_mod_a, b_div_a, b, a );
+  ak_128_div( b_div_a, b_mod_a, b, a );
 
   ak_128_gcd( b_mod_a, a, x1, y1 );
   ak_uint64 b_div_a_mul_x1[4];
   ak_128_mul( b_div_a_mul_x1, b_div_a, x1 );
-  ak_mpzn_sub( x, y1, b_div_a_mul_x1, 4 );
-  for (int i = 0; i < 4; ++i)
-    y[i] = x1[i];
+  ak_128_sub( x, y1, b_div_a_mul_x1 );
+  ak_mpzn_set(y, x1, ak_mpzn256_size);
+//  for (int i = 0; i < 4; ++i)
+//    y[i] = x1[i];
 }
 
- void ak_128_opposite( ak_uint64 *o, const ak_uint64 *x, const ak_uint64 *p )
+/* ----------------------------------------------------------------------------------------------- */
+/*! Функция получает обратный элемент в кольце вычетов по 128-битный модулю. Результат помещается
+    в переменную o. Указатель на r не может совпадать с указателями на исходное число и модуль.
+    Предполагается, что p >= 2^127.
+
+    @param o Указатель на вычет, в который помещается обратный элемент
+    @param x Указатель на вычет, обратный элемент от которого требуется взять
+    @param p Модуль, по которому производятся вычисления                                           */
+/* ----------------------------------------------------------------------------------------------- */
+ inline void ak_128_inverse( ak_uint64 *o, const ak_uint64 *x, const ak_uint64 *p )
 {
-  ak_uint64 x4[4] = { x[0], x[1], 0, 0 };
-  ak_uint64 p4[4] = { p[0], p[1], 0, 0 };
-  ak_uint64 ans[4];
-  ak_uint64 tmp[4];
+  ak_mpzn256 x4 = { x[0], x[1], 0, 0 };
+  ak_mpzn256 p4 = { p[0], p[1], 0, 0 };
+  ak_mpzn256 ans;
+  ak_mpzn256 tmp;
 
   ak_128_gcd( x4, p4, ans, tmp );
   o[0] = ans[0];
   o[1] = ans[1];
 }
 
- static ak_uint64 sub_128( ak_uint64 *z, const ak_uint64 *x, const ak_uint64 *y )
-{
-  z[0] = x[0] - y[0];
-  z[1] = x[1] - y[1];
-  ak_uint64 ans = z[1] > x[1];
-  ak_uint64 before = z[1];
-  z[1] -= z[0] > x[0];
-  ans |= before < z[1];
-  return ans;
-}
-
- static void sub_128_mod( ak_uint64 *z, const ak_uint64 *x, const ak_uint64 *y, const ak_uint64 *p )
-{
-  if ( sub_128( z, x, y ) )
-  {
-    z[0] += p[0];
-    z[1] += p[1];
-    z[1] += p[0] > z[0];
-  }
-}
-
- void ak_128_double_point( ak_uint64 *x3, ak_uint64 *y3, const ak_uint64 *x1, const ak_uint64 *y1, const ak_uint64 *p )
+ void ak_128_point_double( ak_point128 c, const ak_point128 a, const ak_uint64 *p )
 {
   // TODO
-  x3[0] = 0;
-  x3[1] = 0;
-  y3[0] = 0;
-  y3[1] = 0;
+  ak_128_set_zero(c->x);
+  ak_128_set_zero(c->y);
 }
 
- void ak_128_add_points( ak_uint64 *x3, ak_uint64 *y3, const ak_uint64 *x1, const ak_uint64 *y1, const ak_uint64 *x2, const ak_uint64 *y2, const ak_uint64 *p )
+ dll_export void ak_128_point_add( ak_point128 c, const ak_point128 a, const ak_point128 b, const ak_uint64 *p )
 {
-  if ( x1[0] == x2[0] && x1[1] == x2[1] )
+  if ( ak_128_equal( a->x, b->x ) )
   {
     ak_uint64 y1_plus_y2[2];
-    ak_128_add_mod( y1_plus_y2, y1, y2, p );
-    if ( y1_plus_y2[0] == 0 && y1_plus_y2[0] == 0 )
+    ak_128_add_mod( y1_plus_y2, a->y, b->y, p );
+    if ( ak_128_is_zero( y1_plus_y2 ) )
     {
-      x3[0] = 0;
-      x3[1] = 0;
-      y3[0] = 0;
-      y3[1] = 0;
+      ak_128_set_zero(c->x);
+      ak_128_set_zero(c->y);
       return;
     }
-    if ( y1[0] == y2[0] && y1[1] == y2[1] )
+    if ( ak_128_equal( a->y, b->y ) )
     {
-      ak_128_double_point(x3, y3, x1, y1, p);
+      ak_128_point_double( c, a, p );
+      return;
     }
   }
-  ak_uint64 num[2];
-  ak_uint64 den[2];
-  sub_128_mod( num, y1, y2, p );
-  sub_128_mod( den, x1, x2, p );
-  ak_uint64 den_opp[2];
-  ak_128_opposite( den_opp, den, p );
-  ak_uint64 s[2];
-  ak_uint64 s_2[2];
-  ak_128_mul_mod( s, num, den_opp, p );
+  ak_mpzn128 num;
+  ak_mpzn128 den;
+  ak_128_sub_mod( num, a->y, b->y, p );
+  ak_128_sub_mod( den, a->x, b->x, p );
+  ak_mpzn128 den_inv;
+  ak_128_inverse( den_inv, den, p );
+  ak_mpzn128 s;
+  ak_mpzn128 s_2;
+  ak_128_mul_mod( s, num, den_inv, p );
   ak_128_mul_mod( s_2, s, s, p );
-  ak_uint64 s_2_minus_x1[2];
-  sub_128_mod( s_2_minus_x1, s_2, x1, p );
-  sub_128_mod( x3, s_2_minus_x1, x2, p );
-  ak_uint64 x3_minus_x1[2];
-  sub_128_mod( x3_minus_x1, x3, x1, p );
-  ak_uint64 s_mul_x3_minus_x1[2];
+  ak_mpzn128 s_2_minus_x1;
+  ak_128_sub_mod( s_2_minus_x1, s_2, a->x, p );
+  ak_128_sub_mod( c->x, s_2_minus_x1, b->x, p );
+  ak_mpzn128 x3_minus_x1;
+  ak_128_sub_mod( x3_minus_x1, c->x, a->x, p );
+  ak_mpzn128 s_mul_x3_minus_x1;
   ak_128_mul_mod( s_mul_x3_minus_x1, s, x3_minus_x1, p );
-  ak_128_add_mod( y3, y1, s_mul_x3_minus_x1, p );
+  ak_128_add_mod( c->y, a->y, s_mul_x3_minus_x1, p );
 }
